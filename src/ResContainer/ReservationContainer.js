383a9231ceb6f4cddb1b0dcf4ad417f0d0892a83
Eduardo <jeduardorjx@gmail.com>
2019-10-17 10:06:54 -0600

    Change files to ResContainer

---
+++ src/ResContainer/ReservationContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ResContainer.scss'
+
+const ResContainer = () => {
+  return (
+    <section>
+    Reservation Container
+    </section>
+  )
+}
+
+export default ResContainer;
