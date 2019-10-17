749b66868ab418266ce9dd925afa9a46ec77321e
Eduardo <jeduardorjx@gmail.com>
2019-10-17 10:32:44 -0600

    Render Card with guest infomation

--- src/ResCard/ResCard.js
+++ src/ResCard/ResCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './ResCard.scss'
 
-const ResCard = () => {
+const ResCard = ({ guest }) => {
   return (
-    <article>
-      Res Card
+    <article id={guest.id}>
+      <h2>{guest.name}</h2>
+      <p>{guest.date}</p>
+      <p>{guest.time}</p>
+      <p>Num. of guests: {guest.number}</p>
+      <button className='cancel-btn' 
+        type='button'>CANCEL</button>
     </article>
   )
 }
