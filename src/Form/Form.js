3ce4475fdde66a574627ace6f3308372e48b4dda
Eduardo <jeduardorjx@gmail.com>
2019-10-17 09:49:48 -0600

    Add all my inputs and button to the form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,7 +4,11 @@ import './Form.scss'
 const Form = () => {
   return(
     <form>
-      Form
+      <input type='text' placeholder='Name' />
+      <input type='text' placeholder='Date (MM/DD)' />
+      <input type='text' placeholder='Time' />
+      <input type='text' placeholder='Number of Guests' />
+      <button type='button'>Make Reservation</button>
     </form>
   )
 }
