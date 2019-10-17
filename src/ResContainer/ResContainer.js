788d7cd30d543f6b70019a0f351827d1fd938509
Eduardo <jeduardorjx@gmail.com>
2019-10-17 10:32:32 -0600

    Pass down guest props to container, map through guest array and render a resCard

--- src/ResContainer/ResContainer.js
+++ src/ResContainer/ResContainer.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './ResContainer.scss'
 import ResCard from '../ResCard/ResCard'
 
-const ResContainer = () => {
+const ResContainer = ({ guests }) => {
+  const reservation = guests.map(guest => {
+    return <ResCard guest={guest} key={guest.id} />
+  })
+
   return (
     <section>
-      <ResCard />
+      {reservation}
     </section>
   )
 }
